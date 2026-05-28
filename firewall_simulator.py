6a4f1c034f172516ed1ee8d5899abccad29b90cb
wyne-ybanez <wyne_01@icloud.com>
2026-05-28 17:51:16 +0100

    init

---
+++ firewall_simulator.py
@@ -0,0 +1,16 @@
+import random
+
+def main():
+    # Define the firewall rules (key: IP address, value: action)
+    firewall_rules = {
+        "192.168.1.1": "block",
+        "192.168.1.4": "block",
+        "192.168.1.9": "block",
+        "192.168.1.13": "block",
+        "192.168.1.16": "block",
+        "192.168.1.19": "block"
+}
+
+if __name__ == "__main__":
+    main()
+
